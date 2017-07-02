0eea1f71cf14947b322624386584b5674d979374
tusharm-cuelogic <tushar.mate@cuelogic.co.in>
2017-07-02 12:11:25 +0530

    Worked on User Sign up, Sign In, User listing and User Profile page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  
+  devise_for :users
+  resources :users, only: [:index, :show]
+  root to: 'static_pages#home'
+  match '/about', to: 'static_pages#about', via: :get
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
